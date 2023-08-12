bdabd0bb9d29ca818014a2010d8d0c8fb53e5360
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-08-12 11:24:10 -0700

    Subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ Queries the Reddit API and returns the number of subscribers for a subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'YourVANnenna'}
@@ -16,12 +17,3 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        print("{:d}".format(number_of_subscribers(subreddit)))
